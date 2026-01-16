8ff942da811abae38beb0238055f837a79564e43
Michael QQ Kong <qkong1@Lenovo.com>
2026-01-16 17:51:48 +0800

    fix linux shell

--- finagent.sh
+++ finagent.sh
@@ -2,16 +2,16 @@
 
 if [ -z "$1" ]; then
     echo "Missing stock symbol"
-    return
+    exit 1
 elif [ -z "$2" ]; then
     echo "Missing period ('short+' within 2 weeks; 'short' from 2 weeks to 1 month; 'medium' from 1 month to 1 year; 'long' from 1 year to 2 years)"
-    return
+    exit 1
 elif [ "$2" != "short+" ] && [ "$2" != "short" ] && [ "$2" != "medium" ] && [ "$2" != "long" ]; then
     echo "Invalid period. Please use 'short+', 'short', 'medium', or 'long'."
-    return
+    exit 1
 else
     # configure your workspace
-    cd /app/workspace/finAgent || return
+    cd /app/workspace/finAgent || exit 1
     source finagent/bin/activate
     python finagent.py --symbol "$1" --period "$2"
     deactivate
