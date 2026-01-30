e6453c95c07294967d43a3fc1d791f267181f011
Michael QQ Kong <qkong1@Lenovo.com>
2026-01-30 18:35:06 +0800

    update timeout of gunicorn

--- start_backend.sh
+++ start_backend.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /app/workspace/finAgent
 source finagent/bin/activate
-exec /app/workspace/finAgent/finagent/bin/gunicorn -w 1 -k uvicorn.workers.UvicornWorker finagent_api:app --bind 0.0.0.0:8000 --timeout 120 --log-level debug
+exec /app/workspace/finAgent/finagent/bin/gunicorn -w 2 -k uvicorn.workers.UvicornWorker finagent_api:app --bind 0.0.0.0:8000 --timeout 480 --log-level debug
